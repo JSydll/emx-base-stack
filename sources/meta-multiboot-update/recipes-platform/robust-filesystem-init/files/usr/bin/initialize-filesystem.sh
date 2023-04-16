d96d514af568994c246fb6fe74e3cc04d23a1817
Joschka Seydell <joschka@seydell.org>
2023-04-16 16:15:46 +0200

    Use an overlay instead of a bind mount for /etc/ssh as there is data in the rootfs.

--- sources/meta-multiboot-update/recipes-platform/robust-filesystem-init/files/usr/bin/initialize-filesystem.sh
+++ sources/meta-multiboot-update/recipes-platform/robust-filesystem-init/files/usr/bin/initialize-filesystem.sh
@@ -3,6 +3,7 @@
 # Sets up the required filesystem structure for overlays and shared data
 #
 
+mkdir -p /var/persistent/shared/overlays/ssh /var/persistent/shared/overlays/ssh-work
+
 mkdir -p /var/persistent/shared/media
-mkdir -p /var/persistent/shared/ssh
 mkdir -p /var/persistent/private/log
